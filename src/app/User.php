a5729c39d635c308a5ae0d5cd34c0a0b8b339402
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-04-21 02:54:15 +0000

    [add] sanctumパッケージを追加した

--- src/app/User.php
+++ src/app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
