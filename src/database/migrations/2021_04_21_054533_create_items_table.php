d9a09066533fb1d75f8052d8459d982814b6c950
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-04-21 05:50:27 +0000

    [add] ItemのMigrationとModel

---
+++ src/database/migrations/2021_04_21_054533_create_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->timestamps();
+            $table->string('name')->index();
+            $table->text('description')->nullable();
+            $table->string('image_path');
+            $table->unsignedBigInteger('node_id')->nullable()->index();
+            $table->foreign('node_id')->references('id')->on('nodes');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
