8a279c51f69fa385008982edb3f4c7aae7d64248
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-04-29 05:31:32 +0000

    [change] Roomに追加した

--- src/app/Room.php
+++ src/app/Room.php
@@ -3,8 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Home;
+use App\Box;
+use App\Item;
 
 class Room extends Model
 {
-    //
+
+    protected $fillable = ['name', 'description', 'home_id'];
+
+    public function home()
+    {
+        return $this->belongsTo(Home::class);
+    }
+
+    public function boxes()
+    {
+        return $this->hasMany(Box::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(Item::class);
+    }
 }
