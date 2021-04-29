f24e61b066c3fe8f2f669cd61d83a861b308d893
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-04-29 06:34:25 +0000

    [change] Factoryを作成しました

---
+++ src/database/factories/BoxFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Box;
+use Faker\Generator as Faker;
+
+$factory->define(Box::class, function (Faker $faker) {
+    return [
+        
+    ];
+});
---
+++ src/database/factories/HomeFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Home;
+use Faker\Generator as Faker;
+
+$factory->define(Home::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
