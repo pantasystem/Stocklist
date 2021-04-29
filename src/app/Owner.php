28cbdc3ed498972642032f56eb7c0c92fd28f837
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-04-29 05:38:08 +0000

    [change] relationを実装したJ

--- src/app/Owner.php
+++ src/app/Owner.php
@@ -2,6 +2,9 @@
 
 namespace App;
 
+use App\Item;
+use App\User;
+
 use Illuminate\Database\Eloquent\Model;
 
 class Owner extends Model
@@ -9,4 +12,14 @@ class Owner extends Model
     //
     protected $primaryKeys = ['user_id', 'item_id'];
     protected $fillable = ['user_id', 'item_id'];
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
