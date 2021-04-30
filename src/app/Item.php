80fa601707013e8c94a4c360d953ff4276e9daa1
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-04-30 07:25:26 +0000

    [fix] Itemのリレーションを修正した

--- src/app/Item.php
+++ src/app/Item.php
@@ -33,10 +33,20 @@ class Item extends Model
         return $this->belongsToMany(User::class);
     }
 
-    
-    public function box()
+
+    /**
+     * Stockを経由してBoxを取得する
+     */
+    public function boxes()
     {
-        return $this->belongsTo(Box::class);
+        return $this->hasManyThrough(
+            Box::class,
+            Stock::class,
+            'item_id', //中継テーブルの自分に対する参照キー
+            'id', // 中間テーブルを参照する取得予定のテーブルのキー
+            'id',
+            'box_id'
+        );
     }
 
     public function stocks()
