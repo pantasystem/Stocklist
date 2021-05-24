b6055f850cef381c8a7908d32d776256c1821e5c
yumekiti <yumekiti120440@gmail.com>
2021-05-24 10:02:42 +0900

    add: UpdateBoxApi

--- src/app/Http/Controllers/Api/BoxController.php
+++ src/app/Http/Controllers/Api/BoxController.php
@@ -5,7 +5,26 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\UpdateBoxRequest;
+
 class BoxController extends Controller
 {
     //
+    public function update(UpdateBoxRequest $request, $box_id)
+    {
+        Auth::user()
+        ->home()
+        ->firstOrFail()
+        ->boxes()
+        ->findOrFail($box_id)
+        ->update(
+            $request->only([
+                'name',
+                'description'
+            ])
+        );
+
+        return response(null,204);
+    }
 }
