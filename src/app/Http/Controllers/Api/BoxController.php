86f42a6740abf97f2cd0d33a35879d2c7122f533
yumekiti <yumekiti120440@gmail.com>
2021-05-24 09:47:53 +0900

    add: CreateBoxApi

--- src/app/Http/Controllers/Api/BoxController.php
+++ src/app/Http/Controllers/Api/BoxController.php
@@ -5,7 +5,26 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\CreateBoxRequest;
+
 class BoxController extends Controller
 {
     //
+    public function create(CreateBoxRequest $request)
+    {
+        $box = Auth::user()
+        ->home()
+        ->firstOrFail()
+        ->boxes();
+
+        $boxcreated = $box ->create(
+            $request->only([
+                'name',
+                'description'
+            ])
+        );
+
+        return $boxcreated;
+    }
 }
--- src/routes/api.php
+++ src/routes/api.php
@@ -43,3 +43,5 @@ Route::get('/categories', 'Api\CategoryController@index')->middleware('auth:sanc
 Route::post('/categories', 'Api\CategoryController@create')->middleware('auth:sanctum');
 Route::put('/categories/{category_id}', 'Api\CategoryController@update')->middleware('auth:sanctum');
 Route::delete('/categories/{category_id}', 'Api\CategoryController@delete')->middleware('auth:sanctum');
+
+Route::post('/boxes', 'Api\BoxController@create')->middleware('auth:sanctum');
