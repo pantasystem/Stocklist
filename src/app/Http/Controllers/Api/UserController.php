43fa1863c00608792585be4138373ac249c52237
yumekiti <yumekiti120440@gmail.com>
2021-05-27 15:44:57 +0900

    add: AddSpa

--- src/app/Http/Controllers/Api/UserController.php
+++ src/app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,32 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Http\Requests\LoginUserRequest;
+
 class UserController extends Controller
 {
     //
+
+    public function login(LoginUserRequest $request)
+    {
+        $credentials = $request->validate([
+            'email'    => 'required|email',
+            'password' => 'required'
+        ]);
+    
+        if (Auth::attempt($credentials)) {
+            return ['message' => 'ログインしました。'];
+        }
+
+        throw ValidationException::withMessages([
+            'email' => ['メールアドレスまたはパスワードが違います。'],
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return ['message' => 'ログアウトしました。'];
+    }
+
 }
