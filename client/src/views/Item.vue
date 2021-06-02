0c0f7dd6849878622089dead144e648294603886
yumekiti <yumekiti120440@gmail.com>
2021-06-02 16:08:52 +0900

    add: nodedocker

---
+++ client/src/views/Item.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-app>
+        
+        <Header />
+
+    </v-app>
+</template>
+
+<script>
+import Header from '@/components/Header.vue'
+
+export default {
+    name: 'Item',
+    components: {
+        Header
+    },
+}
+</script>
--- docker/php/php.sh
+++ docker/php/php.sh
@@ -2,11 +2,11 @@ flag=true
 
 while $flag; do
 
-    echo "wait"
-    sleep 1
+  sleep 1
 
-    if (docker-compose logs | grep bind-address > /dev/null 2>&1) ; then
-		flag=false
-	fi
+  if (docker-compose logs | grep bind-address > /dev/null 2>&1) ; then
+    flag=false
+    echo "mysql ready"
+  fi
 
 done
