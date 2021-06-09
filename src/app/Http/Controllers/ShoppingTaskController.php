d878de0d8df60a889890acc0ecdb5cd828eb8642
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-09 13:37:39 +0000

    [change] validationを実装した

---
+++ src/app/Http/Controllers/ShoppingTaskController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ShoppingTaskController extends Controller
+{
+    //
+    public function create()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function delete()
+    {
+
+    }
+
+    public function complete()
+    {
+
+    }
+
+    public function incomplete()
+    {
+        
+    }
+}
---
+++ src/app/Http/Requests/UpdateShoppingTaskRequest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rule;
+
+class UpdateShoppingTaskRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'item_id' => ['required', Rule::exists('items', 'id')->where(function($query){
+                $homeId = Auth::user()->home_id;
+                $query->where('home_id', '=', $homeId);
+            })],
+            'count' => ['required', 'integer', 'numeric', 'min:0'],
+            'completed_at' => ['date', 'nullable'],
+            'shopping_list_id' => ['required', Rule::exists('shopping_lists', 'id')->where(function($query){
+                $homeId = Auth::user()->home_id;
+                $query->where('home_id', '=', $homeId);
+            })],
+            'box_id' => ['required', Rule::exists('shopping_lists', 'id')->where(function($query){
+                $homeId = Auth::user()->home_id;
+                $query->where('home_id', '=', $homeId);
+            })]
+        ];
+
+    }
+}
