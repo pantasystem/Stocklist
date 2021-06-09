5eba47d8629cd75eb77a4a57e928661125d9b951
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-09 07:41:35 +0000

    [change] Requestを実装した

--- src/app/Http/Requests/CreateShoppingListRequest.php
+++ src/app/Http/Requests/CreateShoppingListRequest.php
@@ -13,7 +13,7 @@ class CreateShoppingListRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class CreateShoppingListRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => ['required', 'max:20', 'string']
         ];
     }
 }
--- src/app/Http/Requests/UpdateShoppingListRequest.php
+++ src/app/Http/Requests/UpdateShoppingListRequest.php
@@ -13,7 +13,7 @@ class UpdateShoppingListRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,7 @@ class UpdateShoppingListRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => ['required', 'max:20', 'string']
         ];
     }
 }
