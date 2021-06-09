3be55ef847cf0f673d9445533b945273274ab962
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-09 07:25:45 +0000

    [chnage] リレーションなどを実装した

--- src/app/ShoppingDetail.php
+++ src/app/ShoppingDetail.php
@@ -3,6 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Item;
+use App\Box;
+use App\ShoppingList;
 
 class ShoppingDetail extends Model
 {
@@ -12,6 +15,29 @@ class ShoppingDetail extends Model
         'count',
         'completed_at',
         'shopping_list_id',
-        'stock_id'
+        'box_id'
     ];
+
+    protected $appends = ['is_completed'];
+    
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
+
+    public function box()
+    {
+        return $this->belongsTo(Box::class);
+    }
+
+    public function shoppingList()
+    {
+        return $this->belongsTo(ShoppingList::class);
+    }
+
+    public function getIsCompletedAttribute()
+    {
+        return $this->completed_at != null;
+    }
+
 }
