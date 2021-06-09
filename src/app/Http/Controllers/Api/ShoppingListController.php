9b4035862d75a8747c441b8f436b95150c60b3ca
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-09 07:40:36 +0000

    [change] indexを実装した

--- src/app/Http/Controllers/Api/ShoppingListController.php
+++ src/app/Http/Controllers/Api/ShoppingListController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateShoppingListRequest;
+use App\Http\Requests\UpdateShoppingListRequest;
+use App\Home;
+use Illuminate\Support\Facades\Auth;
 
 class ShoppingListController extends Controller
 {
@@ -11,15 +15,15 @@ class ShoppingListController extends Controller
 
     public function index()
     {
-
+        Auth::user()->home()->first()->shoppingLists()->get();
     }
 
-    public function update()
+    public function update(UpdateShoppingListRequest $request, $shoppingListId)
     {
 
     }
 
-    public function create()
+    public function create(CreateShoppingListRequest $request)
     {
 
     }
