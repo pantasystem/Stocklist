85b1f0c9cefe5d14663e7226fe37f0c079c7dea1
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-09 13:18:23 +0000

    [change] 名前を変更した

--- src/database/migrations/2021_05_27_071946_create_shopping_tasks_table.php
+++ src/database/migrations/2021_05_27_071946_create_shopping_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateShoppingDetailTable extends Migration
+class CreateShoppingTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -33,6 +33,6 @@ class CreateShoppingDetailTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('shopping_details');
+        Schema::dropIfExists('shopping_tasks');
     }
 }
