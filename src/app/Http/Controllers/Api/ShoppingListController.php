510c4302a1c6cf4dada8341f06023315c431bd85
Yuichiro Kinoshita <kinoshita062323@gmail.com>
2021-06-09 07:34:51 +0000

    [add] ShoppingListControllerを作成した

---
+++ src/app/Http/Controllers/Api/ShoppingListController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ShoppingListController extends Controller
+{
+    //
+
+    public function index()
+    {
+
+    }
+
+    public function update()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function delete($shoppingListId)
+    {
+
+    }
+
+    public function show($shoppingListId)
+    {
+
+    }
+}
