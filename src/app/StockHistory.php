c0e48f09dfa76c7c5619d3da94f369f950d5e889
vagrant <vagrant@vagrant.vm>
2021-06-10 07:54:54 +0000

    add:StockHistoryよくわからん

---
+++ src/app/StockHistory.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Stock;
+
+class StockHistory extends Model
+{
+    //
+    public function changeCount()
+    {
+        if($oldcount != count){
+            return $timestamps;
+        }
+        $oldcount = count;
+    }
+}
