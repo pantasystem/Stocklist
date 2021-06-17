3babe8a27dd89a80de9bbe94271c5a6360ccd066
vagrant <vagrant@vagrant.vm>
2021-06-17 07:40:29 +0000

    add:StockHistory途中

---
+++ src/app/Observers/StockObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers;
+
+use App\Stock;
+
+class StockObserver
+{
+    /**
+     * Handle the stock "created" event.
+     *
+     * @param  \App\Stock  $stock
+     * @return void
+     */
+    public function created(Stock $stock)
+    {
+        //
+    }
+
+    /**
+     * Handle the stock "updated" event.
+     *
+     * @param  \App\Stock  $stock
+     * @return void
+     */
+    public function updated(Stock $stock)
+    {
+        //
+    }
+
+    /**
+     * Handle the stock "deleted" event.
+     *
+     * @param  \App\Stock  $stock
+     * @return void
+     */
+    public function deleted(Stock $stock)
+    {
+        //
+    }
+
+    /**
+     * Handle the stock "restored" event.
+     *
+     * @param  \App\Stock  $stock
+     * @return void
+     */
+    public function restored(Stock $stock)
+    {
+        //
+    }
+
+    /**
+     * Handle the stock "force deleted" event.
+     *
+     * @param  \App\Stock  $stock
+     * @return void
+     */
+    public function forceDeleted(Stock $stock)
+    {
+        //
+    }
+}
