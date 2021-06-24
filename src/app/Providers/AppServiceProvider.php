92d02feff26a1708662252848ae6c7e8d79663a5
vagrant <vagrant@vagrant.vm>
2021-06-24 06:02:40 +0000

    add:StockHistory途中

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -28,6 +28,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Home::observe(HomeObserver::class);
-        Stock:observe(StockObserver::class);
+        Stock::observe(StockObserver::class);
     }
 }
